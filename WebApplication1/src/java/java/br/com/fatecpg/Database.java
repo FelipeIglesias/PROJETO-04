234b9371120f39103656595ee51bddc2a57381cf
Felipe Reis <felipe.s.reis@live.com>
2017-04-28 21:49:46 -0300

    Renomeado a classe Clientes para Cliente (singular) por boas práticas

--- WebApplication1/src/java/java/br/com/fatecpg/Database.java
+++ WebApplication1/src/java/java/br/com/fatecpg/Database.java
@@ -18,18 +18,18 @@ public class Database {
         return fornecedores;
     }
     
-    private static ArrayList<Clientes> clientes;
-    public static ArrayList<Clientes> getClientes(){
-        if(clientes == null){
-            clientes = new  ArrayList<>();
-            Clientes c1 = new Clientes();
-            c1.setClientes("Pedrinho","999.888.777-66","99.888.777-6","pedrinho@email.com","1111-2222","Rua Um");
-            Clientes c2 = new Clientes();
-            c2.setClientes("Joãozinho","111.222.333-44","22.333.444-5","joaozinho@email.com","1234-5678","Rua Dois");
-            clientes.add(c1);
-            clientes.add(c2);
+    private static ArrayList<Cliente> cliente;
+    public static ArrayList<Cliente> getCliente(){
+        if(cliente == null){
+            cliente = new  ArrayList<>();
+            Cliente c1 = new Cliente();
+            c1.setCliente("Pedrinho","999.888.777-66","99.888.777-6","pedrinho@email.com","1111-2222","Rua Um");
+            Cliente c2 = new Cliente();
+            c2.setCliente("Joãozinho","111.222.333-44","22.333.444-5","joaozinho@email.com","1234-5678","Rua Dois");
+            cliente.add(c1);
+            cliente.add(c2);
         }
-        return clientes;
+        return cliente;
     }
     
     
