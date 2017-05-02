55212433cc364cd876ded97591ea1b14ad845e93
Felipe Reis <felipe.s.reis@live.com>
2017-05-02 01:17:29 -0300

    Indentação e formatação do código.

--- WebApplication1/src/java/br/com/fatecpg/cadastro/Database.java
+++ WebApplication1/src/java/br/com/fatecpg/cadastro/Database.java
@@ -1,8 +1,6 @@
 package br.com.fatecpg.cadastro;
-
 import java.util.ArrayList;
 
-/*@author Felipe*/
 public class Database {
     public static Fornecedores forn;
     public static int pegaIndiceF;
@@ -11,16 +9,19 @@ public class Database {
         if(fornecedores == null){
             fornecedores = new  ArrayList<>();
             Fornecedores f1 = new Fornecedores();
-            f1.setFornecedores("Novinho Alimentos LTDA","Novinho","00.776.574/0001-56","novpg@gmail.com","13 3455-6697","Praça 19 de Janeiro");
+            f1.setFornecedores("Novinho Alimentos LTDA","Novinho","00.776.574/0001-56",
+                    "novpg@gmail.com","13 3455-6697","Praça 19 de Janeiro");
             Fornecedores f2 = new Fornecedores();
-            f2.setFornecedores("Olatunde Eventos LTDA","Olatunde Eventos","00.766.533/0001-51","oooevent@gmail.com","13 3466-9997","MALU");
+            f2.setFornecedores("Olatunde Eventos LTDA","Olatunde Eventos",
+                    "00.766.533/0001-51","oooevent@gmail.com","13 3466-9997","MALU");
             fornecedores.add(f1);
             fornecedores.add(f2);
         }
         return fornecedores;
     }
     
-    public static Fornecedores alteraFornecedor(Fornecedores f, String nome, String cnpj, String razaoSocial, String email, String telefone, String endereco){     
+    public static Fornecedores alteraFornecedor(Fornecedores f, String nome, 
+            String cnpj, String razaoSocial, String email, String telefone, String endereco){     
         f.setNome(nome);
         f.setCnpj(cnpj);
         f.setRazaoSocial(razaoSocial);
@@ -39,16 +40,19 @@ public class Database {
         if(cliente == null){
             cliente = new  ArrayList<>();
             Cliente c1 = new Cliente();
-            c1.setCliente("Pedrinho","999.888.777-66","99.888.777-6","pedrinho@email.com","1111-2222","Rua Um");
+            c1.setCliente("Pedrinho","999.888.777-66","99.888.777-6",
+                    "pedrinho@email.com","1111-2222","Rua Um");
             Cliente c2 = new Cliente();
-            c2.setCliente("Joãozinho","111.222.333-44","22.333.444-5","joaozinho@email.com","1234-5678","Rua Dois");
+            c2.setCliente("Joãozinho","111.222.333-44","22.333.444-5",
+                    "joaozinho@email.com","1234-5678","Rua Dois");
             cliente.add(c1);
             cliente.add(c2);
         }
         return cliente;
     }
     
-    public static Cliente alteraCliente(Cliente c, String nome, String cpf, String rg, String email, String telefone, String endereco){     
+    public static Cliente alteraCliente(Cliente c, String nome, String cpf, 
+            String rg, String email, String telefone, String endereco){     
         c.setNome(nome);
         c.setCpf(cpf);
         c.setRg(rg);
@@ -58,5 +62,4 @@ public class Database {
         
         return c;
     }
-    
 }
--- WebApplication1/src/java/br/com/fatecpg/cadastro/Fornecedores.java
+++ WebApplication1/src/java/br/com/fatecpg/cadastro/Fornecedores.java
@@ -1,6 +1,5 @@
 package br.com.fatecpg.cadastro;
 
-/*@author Felipe */
 public class Fornecedores {
     private String nome;
     private String razaoSocial;
@@ -9,7 +8,8 @@ public class Fornecedores {
     private String telefone;
     private String endereco;
 
-    public void setFornecedores(String nome, String razaoSocial, String cnpj, String email, String telefone, String endereco) {
+    public void setFornecedores(String nome, String razaoSocial, String cnpj, 
+            String email, String telefone, String endereco) {
         this.setNome(nome);
         this.setRazaoSocial(razaoSocial);
         this.setCnpj(cnpj);
@@ -18,67 +18,51 @@ public class Fornecedores {
         this.setEndereco(endereco);
     }
 
-    
-    
-    
     public String getNome() {
         return nome;
     }
-
     
     public void setNome(String nome) {
         this.nome = nome;
     }
 
-    
     public String getRazaoSocial() {
         return razaoSocial;
     }
 
-    
     public void setRazaoSocial(String razaoSocial) {
         this.razaoSocial = razaoSocial;
     }
 
-    
     public String getCnpj() {
         return cnpj;
     }
 
-    
     public void setCnpj(String cnpj) {
         this.cnpj = cnpj;
     }
 
-    
     public String getEmail() {
         return email;
     }
 
-    
     public void setEmail(String email) {
         this.email = email;
     }
 
-    
     public String getTelefone() {
         return telefone;
     }
 
-    
     public void setTelefone(String telefone) {
         this.telefone = telefone;
     }
 
-    
     public String getEndereco() {
         return endereco;
     }
 
-    
     public void setEndereco(String endereco) {
         this.endereco = endereco;
     }
-    
-    
 }
